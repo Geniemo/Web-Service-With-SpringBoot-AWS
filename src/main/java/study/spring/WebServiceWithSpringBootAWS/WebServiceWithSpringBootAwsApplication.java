ff1d27524933233bc04a2380af3d88c3ae3339c1
geniemo <kor98won@gmail.com>
2023-01-11 21:05:22 +0900

    프로젝트 생성

---
+++ src/main/java/study/spring/WebServiceWithSpringBootAWS/WebServiceWithSpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package study.spring.WebServiceWithSpringBootAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceWithSpringBootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceWithSpringBootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/spring/WebServiceWithSpringBootAWS/WebServiceWithSpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.WebServiceWithSpringBootAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceWithSpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
