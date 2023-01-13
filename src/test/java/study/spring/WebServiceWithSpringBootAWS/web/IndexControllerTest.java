9fc483224d4dba55f273fba3b2191da118d38308
geniemo <kor98won@gmail.com>
2023-01-13 17:12:13 +0900

    4.2 기본 페이지 만들기

---
+++ src/test/java/study/spring/WebServiceWithSpringBootAWS/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package study.spring.WebServiceWithSpringBootAWS.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadMainPage() throws Exception {
+        // when
+        String body = restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
